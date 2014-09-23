4a804d877760c2d4ec1c9dd53725be824feda868
namjun <npar350@auckland.ac.nz>
2014-09-24 03:25:23 +1200

    check exitvalue after each process to check if it finished peacefully

--- vamix/src/a03/TitleCreditGenerator.java
+++ vamix/src/a03/TitleCreditGenerator.java
@@ -64,10 +64,15 @@ public class TitleCreditGenerator extends SwingWorker<Integer, String> implement
 			publish("Creating video from image...");
 			//if cancel button has been pressed
 			if (isCancelled()){
-				//destroy process
+				//destroy process and return exit value
 				process.destroy();
-				break;
+				int exitValue = process.waitFor();
+				return exitValue;
 			}
+		}
+		//if process hasn't finished happily, return exit value which will be non zero
+		if (process.waitFor() != 0){
+			return process.waitFor();
 		}
 		
 		//terminal command to add 10sec and music to the 10sec video
@@ -84,10 +89,15 @@ public class TitleCreditGenerator extends SwingWorker<Integer, String> implement
 			publish("adding music and text to video...");
 			//if cancel button has been pressed
 			if (isCancelled()){
-				//destroy process
-				process.destroy();
-				break;
+				//destroy process and return exit value
+				process2.destroy();
+				int exitValue = process2.waitFor();
+				return exitValue;
 			}
+		}
+		//if process hasn't finished happily, return exit value which will be non zero
+		if (process2.waitFor() != 0){
+			return process2.waitFor();
 		}
 		
 		if (_titleOrCredit == true) {
@@ -106,10 +116,15 @@ public class TitleCreditGenerator extends SwingWorker<Integer, String> implement
 				publish("encoding title page(s)...");
 				//if cancel button has been pressed
 				if (isCancelled()){
-					//destroy process
-					process.destroy();
-					break;
+					//destroy process and return exit value
+					process3.destroy();
+					int exitValue = process3.waitFor();
+					return exitValue;
 				}
+			}
+			//if process hasn't finished happily, return exit value which will be non zero
+			if (process3.waitFor() != 0){
+				return process3.waitFor();
 			}
 			
 			//terminal command to turn main vid into .ts
@@ -126,10 +141,15 @@ public class TitleCreditGenerator extends SwingWorker<Integer, String> implement
 				publish("encoding main video... \nThis process may take a few minutes");
 				//if cancel button has been pressed
 				if (isCancelled()){
-					//destroy process
-					process.destroy();
-					break;
+					//destroy process and return exit value
+					process4.destroy();
+					int exitValue = process4.waitFor();
+					return exitValue;
 				}
+			}
+			//if process hasn't finished happily, return exit value which will be non zero
+			if (process4.waitFor() != 0){
+				return process4.waitFor();
 			}
 			
 			//terminal command to concat the 2 .ts files and turn into mp4.
@@ -146,10 +166,15 @@ public class TitleCreditGenerator extends SwingWorker<Integer, String> implement
 				publish("adding title page to video...");
 				//if cancel button has been pressed
 				if (isCancelled()){
-					//destroy process
-					process.destroy();
-					break;
+					//destroy process and return exit value
+					process5.destroy();
+					int exitValue = process5.waitFor();
+					return exitValue;
 				}
+			}
+			//if process hasn't finished happily, return exit value which will be non zero
+			if (process5.waitFor() != 0){
+				return process5.waitFor();
 			}
 			
 		} else {
@@ -168,10 +193,15 @@ public class TitleCreditGenerator extends SwingWorker<Integer, String> implement
 				publish("encoding credit page(s)...");
 				//if cancel button has been pressed
 				if (isCancelled()){
-					//destroy process
-					process.destroy();
-					break;
+					//destroy process and return exit value
+					process3.destroy();
+					int exitValue = process3.waitFor();
+					return exitValue;
 				}
+			}
+			//if process hasn't finished happily, return exit value which will be non zero
+			if (process3.waitFor() != 0){
+				return process3.waitFor();
 			}
 			
 			//terminal command to turn main vid into .ts
@@ -188,10 +218,15 @@ public class TitleCreditGenerator extends SwingWorker<Integer, String> implement
 				publish("encoding main video... This process may take a few minutes");
 				//if cancel button has been pressed
 				if (isCancelled()){
-					//destroy process
-					process.destroy();
-					break;
+					//destroy process and return exit value
+					process4.destroy();
+					int exitValue = process4.waitFor();
+					return exitValue;
 				}
+			}
+			//if process hasn't finished happily, return exit value which will be non zero
+			if (process4.waitFor() != 0){
+				return process4.waitFor();
 			}
 			
 			//terminal command to concat the 2 .ts files and turn into mp4.
@@ -208,10 +243,15 @@ public class TitleCreditGenerator extends SwingWorker<Integer, String> implement
 				publish("adding credit page to video...");
 				//if cancel button has been pressed
 				if (isCancelled()){
-					//destroy process
-					process.destroy();
-					break;
+					//destroy process and return exit value
+					process5.destroy();
+					int exitValue = process5.waitFor();
+					return exitValue;
 				}
+			}
+			//if process hasn't finished happily, return exit value which will be non zero
+			if (process5.waitFor() != 0){
+				return process5.waitFor();
 			}
 			
 		}
@@ -223,7 +263,9 @@ public class TitleCreditGenerator extends SwingWorker<Integer, String> implement
 		
 		try {
 			if (this.get() == 0) {
-				JOptionPane.showMessageDialog(null,"Video edit successful!");
+				JOptionPane.showMessageDialog(null,"Done!");
+			} else {
+				JOptionPane.showMessageDialog(null,"Error!");
 			}
 		} catch (InterruptedException | ExecutionException e) {
 			// TODO Auto-generated catch block
