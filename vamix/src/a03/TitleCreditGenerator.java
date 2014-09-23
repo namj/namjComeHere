1727ec485922fa0fea02d4d18ed60fc1c7c71b2c
namjun <npar350@auckland.ac.nz>
2014-09-24 02:34:31 +1200

    added responsive frame that informs about the progress

--- vamix/src/a03/TitleCreditGenerator.java
+++ vamix/src/a03/TitleCreditGenerator.java
@@ -1,20 +1,26 @@
 package a03;
 
+import java.awt.Color;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.SwingWorker;
 
-public class TitleCreditGenerator extends SwingWorker<Integer, Integer> {
+public class TitleCreditGenerator extends SwingWorker<Integer, String> {
 	
 	private boolean _titleOrCredit; // true indicates title, false indicates credit
 	private String _text;
 	private String _musicPath;
 	private String _imagePath;
 	private String _videoPath;
+	private JFrame _frame;
+	private JLabel _progressText;
 	
 
 	public TitleCreditGenerator(boolean titleOrCredit, String text, String music, String image, String path){
@@ -23,6 +29,16 @@ public class TitleCreditGenerator extends SwingWorker<Integer, Integer> {
 		_musicPath = music;
 		_imagePath = image;
 		_videoPath = path;
+		
+		_frame = new JFrame();
+		_progressText = new JLabel("encoding...");
+		
+		_frame.setBackground(Color.LIGHT_GRAY);
+		_frame.setSize(300,150);
+		_frame.add(_progressText);
+		_frame.setVisible(true);
+		
+		
 	}
 	
 	@Override
@@ -36,8 +52,10 @@ public class TitleCreditGenerator extends SwingWorker<Integer, Integer> {
 		InputStream stdout = process.getInputStream();
 		BufferedReader stdoutB = new BufferedReader(new InputStreamReader(stdout));
 		String line = null;
+		//print output from terminal to console
 		while ((line = stdoutB.readLine()) != null) {
 			System.out.println(line);
+			publish("Creating video from image...");
 		}
 		
 		//terminal command to add 10sec and music to the 10sec video
@@ -48,8 +66,10 @@ public class TitleCreditGenerator extends SwingWorker<Integer, Integer> {
 		InputStream stdoutC = process2.getInputStream();
 		BufferedReader stdoutD = new BufferedReader(new InputStreamReader(stdoutC));
 		line = null;
+		//print output from terminal to console
 		while ((line = stdoutD.readLine()) != null) {
 			System.out.println(line);
+			publish("adding music and text to video...");
 		}
 		
 		if (_titleOrCredit == true) {
@@ -62,8 +82,10 @@ public class TitleCreditGenerator extends SwingWorker<Integer, Integer> {
 			InputStream stdoutE = process3.getInputStream();
 			BufferedReader stdoutF = new BufferedReader(new InputStreamReader(stdoutE));
 			line = null;
+			//print output from terminal to console
 			while ((line = stdoutF.readLine()) != null) {
 				System.out.println(line);
+				publish("encoding title page(s)...");
 			}
 			
 			//terminal command to turn main vid into .ts
@@ -74,8 +96,10 @@ public class TitleCreditGenerator extends SwingWorker<Integer, Integer> {
 			InputStream stdoutG = process4.getInputStream();
 			BufferedReader stdoutH = new BufferedReader(new InputStreamReader(stdoutG));
 			line = null;
+			//print output from terminal to console
 			while ((line = stdoutH.readLine()) != null) {
 				System.out.println(line);
+				publish("encoding main video... \nThis process may take a few minutes");
 			}
 			
 			//terminal command to concat the 2 .ts files and turn into mp4.
@@ -86,8 +110,10 @@ public class TitleCreditGenerator extends SwingWorker<Integer, Integer> {
 			InputStream stdoutI = process5.getInputStream();
 			BufferedReader stdoutJ = new BufferedReader(new InputStreamReader(stdoutI));
 			line = null;
+			//print output from terminal to console
 			while ((line = stdoutJ.readLine()) != null) {
 				System.out.println(line);
+				publish("adding title page to video...");
 			}
 			
 		} else {
@@ -100,8 +126,10 @@ public class TitleCreditGenerator extends SwingWorker<Integer, Integer> {
 			InputStream stdoutE = process3.getInputStream();
 			BufferedReader stdoutF = new BufferedReader(new InputStreamReader(stdoutE));
 			line = null;
+			//print output from terminal to console
 			while ((line = stdoutF.readLine()) != null) {
 				System.out.println(line);
+				publish("encoding credit page(s)...");
 			}
 			
 			//terminal command to turn main vid into .ts
@@ -112,8 +140,10 @@ public class TitleCreditGenerator extends SwingWorker<Integer, Integer> {
 			InputStream stdoutG = process4.getInputStream();
 			BufferedReader stdoutH = new BufferedReader(new InputStreamReader(stdoutG));
 			line = null;
+			//print output from terminal to console
 			while ((line = stdoutH.readLine()) != null) {
 				System.out.println(line);
+				publish("encoding main video... This process may take a few minutes");
 			}
 			
 			//terminal command to concat the 2 .ts files and turn into mp4.
@@ -124,8 +154,10 @@ public class TitleCreditGenerator extends SwingWorker<Integer, Integer> {
 			InputStream stdoutI = process5.getInputStream();
 			BufferedReader stdoutJ = new BufferedReader(new InputStreamReader(stdoutI));
 			line = null;
+			//print output from terminal to console
 			while ((line = stdoutJ.readLine()) != null) {
 				System.out.println(line);
+				publish("adding credit page to video...");
 			}
 			
 		}
@@ -137,12 +169,20 @@ public class TitleCreditGenerator extends SwingWorker<Integer, Integer> {
 		
 		try {
 			if (this.get() == 0) {
-				JOptionPane.showMessageDialog(null,"Title page(s) generation successful!");
+				JOptionPane.showMessageDialog(null,"Video edit successful!");
 			}
 		} catch (InterruptedException | ExecutionException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+	}
+	
+	@Override
+	protected void process(List<String> chunks) {
+		
+		for (int i = 0 ; i < chunks.size() ; i ++ )
+		_progressText.setText(chunks.get(i));
 		
 	}
 	
