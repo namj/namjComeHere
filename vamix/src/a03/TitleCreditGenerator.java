1ce597d70e30948c2222d7adf73be9c3a97fbbf8
namjun <npar350@auckland.ac.nz>
2014-09-24 23:00:20 +1200

    added Logger class that will handle communicating with log file that will track last session in editing title/credit page

--- vamix/src/a03/TitleCreditGenerator.java
+++ vamix/src/a03/TitleCreditGenerator.java
@@ -1,5 +1,6 @@
 package a03;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -18,11 +19,11 @@ import javax.swing.SwingWorker;
 public class TitleCreditGenerator extends SwingWorker<Integer, String> implements ActionListener {
 	
 	private boolean _titleOrCredit; // true indicates title, false indicates credit
-	private String _text;
-	private String _musicPath;
-	private String _imagePath;
-	private String _videoPath;
-	private String _savePath;
+	private String _text; //text to apply
+	private String _musicPath; //path to musicfile
+	private String _imagePath; //path to image to make short video out of
+	private String _videoPath; //path to main video
+	private String _savePath; //path to save videos to
 	
 	private JFrame _frame;
 	private JLabel _progressText;
@@ -30,6 +31,7 @@ public class TitleCreditGenerator extends SwingWorker<Integer, String> implement
 	private boolean _isCancelled = false;
 	
 
+	//constructor for this swingworker
 	public TitleCreditGenerator(boolean titleOrCredit, String text, String music, String image, String path, String savePath){
 		_titleOrCredit = titleOrCredit;
 		_text = text;
@@ -44,9 +46,11 @@ public class TitleCreditGenerator extends SwingWorker<Integer, String> implement
 		_cancelButton.addActionListener(this);
 		
 		_frame.setBackground(Color.LIGHT_GRAY);
+		_frame.setLayout(new BorderLayout());
+		_frame.setLocation(600,400);
 		_frame.setSize(300,150);
-		_frame.add(_progressText);
-		_frame.add(_cancelButton);
+		_frame.add(_progressText,BorderLayout.NORTH);
+		_frame.add(_cancelButton,BorderLayout.SOUTH);
 		_frame.setVisible(true);
 		
 	}
@@ -80,7 +84,7 @@ public class TitleCreditGenerator extends SwingWorker<Integer, String> implement
 		}
 		
 		//terminal command to add 10sec and music to the 10sec video
-		cmd = "avconv -i videoFromImage.mp4 -i "+ _musicPath +" -c:a copy -t 10 -vf \"drawtext=fontcolor=white:fontsize=30:fontfile=/usr/share/fonts/truetype/ubuntu-font-family/Ubuntu-L.ttf:text='"+ _text +"':x=30:y=h-text_h-30\" -y "+ _savePath +"/titleCreditPage.mp4";
+		cmd = "avconv -i "+ _savePath +"/videoFromImage.mp4 -i "+ _musicPath +" -c:a copy -t 10 -vf \"drawtext=fontcolor=white:fontsize=30:fontfile=/usr/share/fonts/truetype/ubuntu-font-family/Ubuntu-L.ttf:text='"+ _text +"':x=30:y=h-text_h-30\" -y "+ _savePath +"/titleCreditPage.mp4";
 		ProcessBuilder Builder2 = new ProcessBuilder("/bin/bash","-c",cmd);
 		Builder2.redirectErrorStream(true);
 		Process process2 = Builder2.start();
@@ -107,7 +111,7 @@ public class TitleCreditGenerator extends SwingWorker<Integer, String> implement
 		if (_titleOrCredit == true) {
 			
 			//terminal command to turn titlepage(openingscene) into .ts 
-			cmd = "avconv -i titleCreditPage.mp4 -vcodec libx264 -acodec aac -bsf:v h264_mp4toannexb -f mpegts -strict experimental -y "+ _savePath +"/file1.ts";
+			cmd = "avconv -i "+ _savePath +"/titleCreditPage.mp4 -vcodec libx264 -acodec aac -bsf:v h264_mp4toannexb -f mpegts -strict experimental -y "+ _savePath +"/file1.ts";
 			ProcessBuilder Builder3 = new ProcessBuilder("/bin/bash","-c",cmd);
 			Builder3.redirectErrorStream(true);
 			Process process3 = Builder3.start();
@@ -157,7 +161,7 @@ public class TitleCreditGenerator extends SwingWorker<Integer, String> implement
 			}
 			
 			//terminal command to concat the 2 .ts files and turn into mp4.
-			cmd = "avconv -i concat:file1.ts\\|file2.ts -c copy -bsf:a aac_adtstoasc -y "+ _savePath +"/Out.mp4";
+			cmd = "avconv -i concat:"+ _savePath +"/file1.ts\\|"+ _savePath +"/file2.ts -c copy -bsf:a aac_adtstoasc -y "+ _savePath +"/Out.mp4";
 			ProcessBuilder Builder5 = new ProcessBuilder("/bin/bash","-c",cmd);
 			Builder5.redirectErrorStream(true);
 			Process process5 = Builder5.start();
@@ -184,7 +188,7 @@ public class TitleCreditGenerator extends SwingWorker<Integer, String> implement
 		} else {
 			
 			//terminal command to turn titlepage(openingscene) into .ts 
-			cmd = "avconv -i titleCreditPage.mp4 -vcodec libx264 -acodec aac -bsf:v h264_mp4toannexb -f mpegts -strict experimental -y "+ _savePath +"/file3.ts";
+			cmd = "avconv -i "+ _savePath +"/titleCreditPage.mp4 -vcodec libx264 -acodec aac -bsf:v h264_mp4toannexb -f mpegts -strict experimental -y "+ _savePath +"/file3.ts";
 			ProcessBuilder Builder3 = new ProcessBuilder("/bin/bash","-c",cmd);
 			Builder3.redirectErrorStream(true);
 			Process process3 = Builder3.start();
@@ -234,7 +238,7 @@ public class TitleCreditGenerator extends SwingWorker<Integer, String> implement
 			}
 			
 			//terminal command to concat the 2 .ts files and turn into mp4.
-			cmd = "avconv -i concat:file2.ts\\|file3.ts -c copy -bsf:a aac_adtstoasc -y "+ _savePath +"/Out.mp4";
+			cmd = "avconv -i concat:"+ _savePath +"/file2.ts\\|"+ _savePath +"/file3.ts -c copy -bsf:a aac_adtstoasc -y "+ _savePath +"/Out.mp4";
 			ProcessBuilder Builder5 = new ProcessBuilder("/bin/bash","-c",cmd);
 			Builder5.redirectErrorStream(true);
 			Process process5 = Builder5.start();
@@ -291,6 +295,7 @@ public class TitleCreditGenerator extends SwingWorker<Integer, String> implement
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == _cancelButton){
 			_isCancelled = true;
+			_frame.dispose();
 		}
 		
 	}
